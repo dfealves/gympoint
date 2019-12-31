332bceb76b3c95ec55f1e8fe879a9f9e8ad5bad0
dfealves <daniloferreira.alves@outlook.com>
2019-12-31 11:11:58 -0300

    config login, need fix

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,35 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
 const schema = Yup.object().shape({
-  email: Yup.string()
-    .email('Insira um e-mail válido')
-    .required('O e-mail é obrigatório'),
-  password: Yup.string().required('A senha é obrigatória'),
+  email: Yup.string().email(),
+  password: Yup.string(),
 });
 
-// import { Container } from './styles';
-
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+  const loading = useSelector(state => state.auth.loading);
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
   }
 
   return (
     <>
-      <img src={logo} alt="GymPoint" />
+      <img src={logo} alt="Gympoint" />
 
       <Form schema={schema} onSubmit={handleSubmit}>
         <text>SEU E-MAIL</text>
         <Input name="email" type="email" placeholder="exemplo@email.com" />
         <text>SUA SENHA</text>
-        <Input name="password" type="password" placeholder="***********" />
+        <Input name="password" type="password" placeholder="*************" />
 
-        <button type="submit">Entrar no sistema</button>
-        {/* <Link></Link> */}
+        <button type="submit">
+          {loading ? 'Carregando...' : 'Entrar no sistema'}
+        </button>
       </Form>
     </>
   );
--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -1,3 +1,36 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
-export default all([]);
+import api from '~/services/api';
+import history from '~/services/history';
+
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+  const { email, password } = payload;
+
+  const response = yield call(api.post, 'sessions', {
+    email,
+    password,
+  });
+
+  const { token, user } = response.data;
+
+  api.defaults.headers.Authorization = `Bearer ${token}`;
+
+  yield put(signInSuccess(token, user));
+
+  history.push('/students');
+}
+
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) api.defaults.headers.Authorization = `Bearer ${token}`;
+}
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+]);
